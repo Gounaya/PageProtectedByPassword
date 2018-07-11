fa8dfe37c55b74fc289667456d37aa32d7fe2215
Gounaya <o.gounaya@gmail.com>
2018-07-11 17:56:07 +0200

    add the firsm formular page

---
+++ formular.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Protected-Page</title>
+    <meta charset ="utf-8"/>
+</head>
+<body>
+    <p>Plese enter the password to obtain access 
+    codes to NASA's server</p>
+    <form action="secret.php" method="post">
+        <p>
+            <input type="password" name="password">
+            <input type="submit" value="Valid">
+        </p>
+    </form>
+</body>
+</html>
