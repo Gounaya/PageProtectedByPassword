9b814f11f2fa914ca0213a89f3883abfae69cfff
Gounaya <o.gounaya@gmail.com>
2018-07-12 22:38:01 +0200

    add the page who destroy session information

---
+++ deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+    session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Déconnexion</title>
+    </head>
+    <body>
+
+    </body>
+</html>
